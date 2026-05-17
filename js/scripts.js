b471442f96134a63ee94ec80d2d33743f2eb0024
Georgy Zaitsev <gnz29042003@mail.ru>
2026-05-17 15:40:29 +0500

    Добавил адаптивную верстку сайта, а также скрипт для открытия видео по кнопке в модальном окне

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 const schemeSvg = document.querySelector('.scheme-svg');
 const totalPriceTag = document.querySelector('.price-total');
+const menuButton = document.querySelector('.m-menu');
+const menu = document.querySelector('.menu');
 let cost = 500;
 let totalPrice = 0;
+
+Fancybox.bind("[data-fancybox]", {
+});
+
 schemeSvg.addEventListener('click', (event) => {
   if(!event.target.classList.contains('booked')) {
     event.target.classList.toggle('active');
@@ -9,4 +15,8 @@ schemeSvg.addEventListener('click', (event) => {
     totalPrice = totalSeats * cost;
     totalPriceTag.textContent = totalPrice;
   }
+})
+
+menuButton.addEventListener('click', () => {
+  menu.classList.toggle('is-open');
 })
